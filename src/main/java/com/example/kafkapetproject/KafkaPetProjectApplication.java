94bc9c357a4b69cc8a4f814af648cfaa31e3d226
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-05-05 18:19:06 +0300

    Init commit

---
+++ src/main/java/com/example/kafkapetproject/KafkaPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkapetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPetProjectApplication.class, args);
+    }
+
+}
